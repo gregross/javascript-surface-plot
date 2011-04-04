2b4dd06e0c3810a4f18ec434fb71bed579a8d170
gregxross@googlemail.com <gregxross@googlemail.com@001a5a74-d991-71f0-78bf-f9e154e01e53>
2011-04-04 22:58:51 +0000

    now rendering axes titles

--- trunk/javascript/SurfacePlot.js
+++ trunk/javascript/SurfacePlot.js
@@ -112,6 +112,10 @@ greg.ross.visualisation.JSSurfacePlot = function(x, y, width, height, colourGrad
 	var xAxisHeader	= "";
 	var yAxisHeader	= "";
 	var zAxisHeader	= "";
+	var xAxisTitleLabel = new greg.ross.visualisation.Tooltip(true);
+	var yAxisTitleLabel = new greg.ross.visualisation.Tooltip(true);
+	var zAxisTitleLabel = new greg.ross.visualisation.Tooltip(true);
+	var tTip = new greg.ross.visualisation.Tooltip(false);
 	
 	function init()
     {
@@ -127,13 +131,13 @@ greg.ross.visualisation.JSSurfacePlot = function(x, y, width, height, colourGrad
 	
 	function hideTooltip()
 	{
-		Tooltip.hide();
+		tTip.hide();
 	}
 	
 	function displayTooltip(e)
 	{
 		var position = new greg.ross.visualisation.Point(e.x, e.y);
-		Tooltip.show(tooltips[closestPointToMouse], 200);
+		tTip.show(tooltips[closestPointToMouse], 200);
 	}
 	
 	function render(data)
@@ -240,6 +244,46 @@ greg.ross.visualisation.JSSurfacePlot = function(x, y, width, height, colourGrad
 		}
 		
 		canvasContext.stroke();
+		
+		renderAxisText(axes);
+	}
+	
+	function renderAxisText(axes)
+	{
+		var xLabelPoint = new greg.ross.visualisation.Point3D(0.0, 0.5, 0.0);
+		var yLabelPoint = new greg.ross.visualisation.Point3D(-0.5, 0.0, 0.0);
+		var zLabelPoint = new greg.ross.visualisation.Point3D(-0.5, 0.5, 0.5);
+		
+		var transformedxLabelPoint = transformation.ChangeObjectPoint(xLabelPoint);
+		var transformedyLabelPoint = transformation.ChangeObjectPoint(yLabelPoint);
+		var transformedzLabelPoint = transformation.ChangeObjectPoint(zLabelPoint);
+		
+		var xAxis = axes[0];
+		var yAxis = axes[1];
+		var zAxis = axes[2];
+		
+		canvasContext.fillStyle = '#fff';
+		
+		if (xAxis.distanceFromCamera > yAxis.distanceFromCamera)
+		{
+			var xAxisLabelPosX = transformedxLabelPoint.ax;
+			var xAxisLabelPosY = transformedxLabelPoint.ay;
+			canvasContext.fillText(xTitle, xAxisLabelPosX, xAxisLabelPosY);
+		}
+		
+		if (xAxis.distanceFromCamera < yAxis.distanceFromCamera)
+		{
+			var yAxisLabelPosX = transformedyLabelPoint.ax;
+			var yAxisLabelPosY = transformedyLabelPoint.ay;
+			canvasContext.fillText(yTitle, yAxisLabelPosX, yAxisLabelPosY);
+		}
+		
+		if (xAxis.distanceFromCamera < zAxis.distanceFromCamera)
+		{
+			var zAxisLabelPosX = transformedzLabelPoint.ax;
+			var zAxisLabelPosY = transformedzLabelPoint.ay;
+			canvasContext.fillText(zTitle, zAxisLabelPosX, zAxisLabelPosY);
+		}
 	}
 	
 	var sort = function(array)
@@ -309,7 +353,7 @@ greg.ross.visualisation.JSSurfacePlot = function(x, y, width, height, colourGrad
 		zAxis.calculateCentroid();
 		zAxis.calculateDistance();
 		axes[axes.length] = zAxis;
-
+		
 		return axes;
 	}
 	
@@ -1060,9 +1104,8 @@ greg.ross.visualisation.Point = function(x, y)
  * This function displays tooltips and was adapted from original code by Michael Leigeber.
  * See http://www.leigeber.com/
  */
-var Tooltip = function(useExplicitPositions)
+greg.ross.visualisation.Tooltip = function(useExplicitPositions)
 {
-	var id = 'tt';
 	var top = 3;
 	var left = 3;
 	var maxw = 300;
@@ -1073,135 +1116,133 @@ var Tooltip = function(useExplicitPositions)
 	var tt,t,c,b,h;
 	var ie = document.all ? true : false;
 	
-	return{
-		show:function(v,w)
+	this.show = function(v,w)
+	{
+		if (tt == null)
 		{
-			if (tt == null)
-			{
-				tt = document.createElement('div');
-				tt.setAttribute('id',id);
-				tt.style.color = "#fff";
-				
-				tt.style.position = 'absolute';
-				tt.style.display =  'block';
-				
-				t = document.createElement('div');
-				t.setAttribute('id',id + 'top');
-				
-				t.style.display = 'block';
-				t.style.height =  '5px';
-				t.style.marginleft =  '5px';
-				t.style.overflow =  'hidden';
-				
-				c = document.createElement('div');
-				c.setAttribute('id',id + 'cont');
-				
-				b = document.createElement('div');
-				b.setAttribute('id',id + 'bot');
-				
-				tt.appendChild(t);
-				tt.appendChild(c);
-				tt.appendChild(b);
-				document.body.appendChild(tt);
-				
-				if (!ie)
-				{
-					tt.style.opacity = 0;
-					tt.style.filter = 'alpha(opacity=0)';
-				}
-				else
-					tt.style.opacity = 1;
-				
-				if (!useExplicitPositions)
-					document.onmousemove = this.pos;
-			}
+			tt = document.createElement('div');
+			tt.style.color = "#fff";
 			
-			tt.style.display = 'block';
-			c.innerHTML = '<span style="font-weight:bold; font-family: arial;">' + v + '</span>';
-			tt.style.width = w ? w + 'px' : 'auto';
+			tt.style.position = 'absolute';
+			tt.style.display =  'block';
 			
-			if (!w && ie)
-			{
-				t.style.display = 'none';
-				b.style.display = 'none';
-				tt.style.width = tt.offsetWidth;
-				t.style.display = 'block';
-				b.style.display = 'block';
-			}
+			t = document.createElement('div');
 			
-			if (tt.offsetWidth > maxw)
-			{
-				tt.style.width = maxw + 'px';
-			}
+			t.style.display = 'block';
+			t.style.height =  '5px';
+			t.style.marginleft =  '5px';
+			t.style.overflow =  'hidden';
 			
-			h = parseInt(tt.offsetHeight) + top;
+			c = document.createElement('div');
+			
+			b = document.createElement('div');
+			
+			tt.appendChild(t);
+			tt.appendChild(c);
+			tt.appendChild(b);
+			document.body.appendChild(tt);
 			
 			if (!ie)
 			{
-				clearInterval(tt.timer);
-				tt.timer = setInterval(function(){Tooltip.fade(1)},timer);
-			}
-		},
-		setPos:function(e)
-		{
-			tt.style.top = e.y + 'px';
-			tt.style.left = e.x + 'px';
-		},
-		pos:function(e)
-		{
-			var u = ie ? event.clientY + document.documentElement.scrollTop : e.pageY;
-			var l = ie ? event.clientX + document.documentElement.scrollLeft : e.pageX;
-			tt.style.top = (u - h) + 'px';
-			tt.style.left = (l + left) + 'px';
-		},
-		fade:function(d)
-		{
-			var a = alpha;
-			
-			if ((a != endalpha && d == 1) || (a != 0 && d == -1))
-			{
-				var i = speed;
-				
-				if (endalpha - a < speed && d == 1)
-				{
-					i = endalpha - a;
-				}
-				else if	(alpha < speed && d == -1)
-				{
-					i = a;
-				}
-			
-			alpha = a + (i * d);
-			tt.style.opacity = alpha * .01;
-			tt.style.filter = 'alpha(opacity=' + alpha + ')';
+				tt.style.opacity = 0;
+				tt.style.filter = 'alpha(opacity=0)';
 			}
 			else
-			{
-				clearInterval(tt.timer);
-				
-				if (d == -1)
-				{
-					tt.style.display = 'none';
-				}
-			}
-		},
-		hide:function()
-		{
-			if (tt == null)
-				return;
+				tt.style.opacity = 1;
+			
+			if (!useExplicitPositions)
+				document.onmousemove = this.pos;
+		}
 		
-			if (!ie)
+		tt.style.display = 'block';
+		c.innerHTML = '<span style="font-weight:bold; font-family: arial;">' + v + '</span>';
+		tt.style.width = w ? w + 'px' : 'auto';
+		
+		if (!w && ie)
+		{
+			t.style.display = 'none';
+			b.style.display = 'none';
+			tt.style.width = tt.offsetWidth;
+			t.style.display = 'block';
+			b.style.display = 'block';
+		}
+		
+		if (tt.offsetWidth > maxw)
+		{
+			tt.style.width = maxw + 'px';
+		}
+		
+		h = parseInt(tt.offsetHeight) + top;
+		
+		if (!ie)
+		{
+			clearInterval(tt.timer);
+			tt.timer = setInterval(function(){fade(1)},timer);
+		}
+	}
+	
+	this.setPos = function(e)
+	{
+		tt.style.top = e.y + 'px';
+		tt.style.left = e.x + 'px';
+	}
+	
+	this.pos = function(e)
+	{
+		var u = ie ? event.clientY + document.documentElement.scrollTop : e.pageY;
+		var l = ie ? event.clientX + document.documentElement.scrollLeft : e.pageX;
+		tt.style.top = (u - h) + 'px';
+		tt.style.left = (l + left) + 'px';
+	}
+	
+	function fade(d)
+	{
+		var a = alpha;
+		
+		if ((a != endalpha && d == 1) || (a != 0 && d == -1))
+		{
+			var i = speed;
+			
+			if (endalpha - a < speed && d == 1)
 			{
-				clearInterval(tt.timer);
-				tt.timer = setInterval(function(){Tooltip.fade(-1)},timer);
+				i = endalpha - a;
 			}
-			else
+			else if	(alpha < speed && d == -1)
+			{
+				i = a;
+			}
+		
+		alpha = a + (i * d);
+		tt.style.opacity = alpha * .01;
+		tt.style.filter = 'alpha(opacity=' + alpha + ')';
+		}
+		else
+		{
+			clearInterval(tt.timer);
+			
+			if (d == -1)
 			{
 				tt.style.display = 'none';
 			}
 		}
-	};
-}();
+	}
+	
+	this.hide = function()
+	{
+		if (tt == null)
+			return;
+	
+		if (!ie)
+		{
+			clearInterval(tt.timer);
+			tt.timer = setInterval(function(){fade(-1)},timer);
+		}
+		else
+		{
+			tt.style.display = 'none';
+		}
+	}
+}
 
 greg.ross.visualisation.JSSurfacePlot.DEFAULT_X_ANGLE	= 47;
 greg.ross.visualisation.JSSurfacePlot.DEFAULT_Z_ANGLE	= 47;
